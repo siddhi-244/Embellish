3285963f5d814840ae20011883b1b6e19a45ae4e
Akkaraju Satvik <satvik.akkaraju@gmail.com>
2021-10-02 14:01:16 +0530

    added comments

--- HTML+CSS+JS/Modals/index.js
+++ HTML+CSS+JS/Modals/index.js
@@ -2,10 +2,12 @@ const modalBackground = document.querySelector('.modalBackground')
 const buttonOpenModal = document.querySelector('#buttonOpen')
 const buttonCloseModal = document.querySelector('#buttonClose')
 
+// Opening Modal
 buttonOpenModal.addEventListener('click', () => {
     modalBackground.classList.add('modalActive')
 })
 
+// Closing Modal
 buttonCloseModal.addEventListener('click', () => {
     modalBackground.classList.remove('modalActive')
 })
