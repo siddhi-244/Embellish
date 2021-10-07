29e96085957785bdd4d9d756d4aa6fb99d3c3a6c
bunny8469 <58695175+bunny8469@users.noreply.github.com>
2021-10-07 16:40:42 +0530

    Added comments

--- HTML+CSS+JS/Buttons/3D-hover-button/script.js
+++ HTML+CSS+JS/Buttons/3D-hover-button/script.js
@@ -7,6 +7,8 @@ function boxEffect(event){
 	let height = event.target.offsetHeight
 	event.target.style.boxShadow = `${abs(x, width/2)*10}px ${abs(y, height/2)*10}px #444`
 	event.target.style.transform = `rotateZ(${abs(x, width/2)*5}deg) rotateX(${abs(y, height/2)*15}deg)`
+	// Here, to creare a 3D effect, the button must behave opposite to the user cursor movement.
+	// So, we are using a abs function to make it negative or positive direction according to the movement
 }
 function abs(x,y){
 	if(x < y){ return 1 } else{ return -1 }
@@ -14,4 +16,5 @@ function abs(x,y){
 function revertState(event){
     event.target.style.boxShadow = '6.5px 6.5px #444'
     event.target.style.transform = 'rotateX(0) rotateZ(0)'
+    // Reverting the state when cursor leaves
 }
