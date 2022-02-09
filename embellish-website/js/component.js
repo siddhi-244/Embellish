adc0e936a51a41eb283d541150aab44e86422186
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-02-09 23:42:43 +0530

    Fetching Components From JSON file

--- embellish-website/js/component.js
+++ embellish-website/js/component.js
@@ -1,26 +1,4 @@
 let componentContainer = document.getElementById('component-container');
-let template = `
-<div class="col-lg-4 mb-5" style="cursor: default;">
-                        <div class="card h-100 shadow border-0">
-                            <div class="card-body p-4">
-                                <div class="badge bg-primary bg-gradient rounded-pill mb-2">News</div>
-                                <div class="text-decoration-none link-dark">
-                                    <div class="h5 card-title mb-3">Blog post title</div>
-                                </div>
-                            </div>
-                            <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
-                                <div class="d-flex align-items-end justify-content-between w-100">
-                                    <div class="d-flex align-items-center w-100">
-                                        <div class="footer-card">
-                                            <a href="/">Preview</a>
-                                            <a class="text-black" href=""><i class="bi bi-github fs-3"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-`
 function fetchComponents() {
     fetch("/embellish-website/js/componentData.json")
         .then(obj => obj.json())
