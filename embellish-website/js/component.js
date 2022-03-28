b299d3f8a484066db47610398870d23e1e82576f
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-03-28 20:50:20 +0530

    Home page random components render

--- embellish-website/js/component.js
+++ embellish-website/js/component.js
@@ -1,13 +1,13 @@
-let componentContainer = document.getElementById('component-container');
+let componentContainer = document.getElementById("component-container");
 function fetchComponents() {
-    fetch("/embellish-website/js/componentData.json")
-        .then(obj => obj.json())
-        .then(jsonData => renderComponents(jsonData));
+  fetch("/embellish-website/js/componentData.json")
+    .then((obj) => obj.json())
+    .then((jsonData) => renderComponents(jsonData));
 }
 function renderComponents(json) {
-    json.items.forEach(component => {
-        let { category, componentName, preview, componentURL } = component;
-        componentContainer.innerHTML += `
+  json.items.forEach((component) => {
+    let { category, componentName, preview, componentURL } = component;
+    componentContainer.innerHTML += `
         <div class="col-lg-4 mb-5" style="cursor: default;">
                             <div class="card h-100 shadow border-0">
                                 <div class="card-body p-4">
@@ -28,7 +28,29 @@ function renderComponents(json) {
                                 </div>
                             </div>
                         </div>
-        `
-    });
+        `;
+  });
 }
-fetchComponents();
+
+if(window.location.pathname.includes("components")){
+    fetchComponents()
+}else{
+    fetchComponentsHome()
+}
+function fetchComponentsHome() {
+  fetch("/embellish-website/js/componentData.json")
+    .then((obj) => obj.json())
+    .then((jsonData) => randomChoose(jsonData));
+}
+
+function randomChoose(json) {
+  min = 0;
+  max = json.items.length;
+  let data = {
+    items: [],
+  };
+  for (let i = 0; i < 3; i++) {
+    data.items.push(json.items[Math.floor(Math.random() * (max - min) + min)]);
+  }
+  renderComponents(data)
+}
