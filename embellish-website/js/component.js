b5934d5f3003712946a9b79727fc657562123321
Suraj Pratap <92919173+SurajPratap10@users.noreply.github.com>
2023-02-02 01:19:00 +0530

    Update component.js

--- embellish-website/js/component.js
+++ embellish-website/js/component.js
@@ -54,3 +54,55 @@ function randomChoose(json) {
   }
   renderComponents(data)
 }
+
+//Autotype JS code:
+
+var dataset = ["Welcome " + " to " + " Embellish "];
+var datasetIndex = 0;
+var data;
+var pause = 1000;
+var addTime = 200;
+var removeTime = 150;
+var letterIndex = 0;
+var currentInterval;
+
+var autoType = document.getElementById("autoType");
+
+function textRotation() {
+  if(datasetIndex == dataset.length) {
+    datasetIndex = 0;
+  };
+  
+  data = dataset[datasetIndex];
+  letterIndex = 0;
+  autoType.className = "";
+  currentInterval = window.setInterval(addLetter, addTime);
+};
+
+function addLetter() {
+  autoType.innerHTML += data.charAt(letterIndex);
+  letterIndex += 1;
+  
+  if(letterIndex > data.length) {
+    autoType.className = "caretAnimation";
+    window.clearInterval(currentInterval);
+    window.setTimeout(startRemove, pause);
+  };
+};
+
+function startRemove() {
+  currentInterval = window.setInterval(removeLetter, removeTime);
+}
+
+function removeLetter() {
+  var currentString = autoType.innerHTML;
+  autoType.innerHTML = currentString.slice(0, -1);
+  
+  if(currentString.length < 1) {
+    window.clearInterval(currentInterval);
+    datasetIndex += 1;
+    textRotation();
+  };
+};
+
+window.onload = window.setTimeout(textRotation, 500);
